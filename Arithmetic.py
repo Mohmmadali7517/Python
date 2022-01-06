d3b8725c13993969ec21529511886b6ec18569ed
Mohmmadali7517 <95292726+Mohmmadali7517@users.noreply.github.com>
2022-01-06 16:35:13 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,22 @@
+
+def Add(iValue1,iValue2):
+	iResult=iValue1+iValue2
+
+	return iResult
+
+def Sub(iValue1,iValue2):
+	iResult=iValue1-iValue2
+
+	return iResult
+
+def Mult(iValue1,iValue2):
+	iResult=iValue1*iValue2
+
+	return iResult
+
+def Div(iValue1,iValue2):
+	iResult=iValue1/iValue2
+
+	return iResult
+
+	
