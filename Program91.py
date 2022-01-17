2cb367e81b30fc23b110825db6ba48164c67e91e
Mohmmadali7517 <95292726+Mohmmadali7517@users.noreply.github.com>
2022-01-17 20:06:22 +0530

    Add files via upload

---
+++ Program91.py
@@ -0,0 +1,31 @@
+"""
+    iRow = 4
+    iCol = 4
+ 
+    #   #   #   #
+    *   *    *   *
+    #   #   #   #
+    *    *    *   *
+"""
+def DisplayPattern(iRows,iCols):
+    for i in range(1,iRows+1):
+        for j in range(1,iCols+1):
+            if((i%2)==0):
+                print("*\t",end='')
+            else:
+                print("#\t",end='')
+        print()
+        
+def main():
+    iValue1,iValue2=0,0
+
+    print("Enter the number of rows")
+    iValue1=int(input())
+
+    print("Enter the number of Cols")
+    iValue2=int(input())
+
+    DisplayPattern(iValue1,iValue2)
+
+if __name__=="__main__":
+    main()
---
+++ Program93.py
@@ -0,0 +1,38 @@
+"""
+    iRow = 6
+
+    iCol = 6
+ 
+    $   #   #   #
+    *   $   #   #
+    *   *   $   #
+    *   *   *   $
+ """
+def DisplayPattern(iRows,iCols):
+
+    if(iRows!=iCols):
+        return
+        
+    for i in range(1,iRows+1):
+        for j in range(1,iCols+1):
+            if(i==j):
+                print("$\t",end='')
+            elif(i>j):
+                print("*\t",end='')
+            else:
+                print("#\t",end='')
+        print()
+
+def main():
+    iValue1,iValue2=0,0
+
+    print("Enter the number of Rows")
+    iValue1=int(input())
+
+    print("Enter the number of Cols")
+    iValue2=int(input())
+
+    DisplayPattern(iValue1,iValue2)
+
+if __name__=="__main__":
+    main()
